ee3dab360e560ab7004939f0a48e8462da4b4ddf
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-06-03 17:17:34 +0800

    prepare before merge

--- src/modules/productCategories/types.ts
+++
@@ -1,5 +0,0 @@
-import { inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from '@/trpc/routers/_app';
-
-export type CategoriesGetOne = inferRouterOutputs<AppRouter>['categories']['getOne'];
-export type CategoriesGetMany = inferRouterOutputs<AppRouter>['categories']['getMany']['items'];
